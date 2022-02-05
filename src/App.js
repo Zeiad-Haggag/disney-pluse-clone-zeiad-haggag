f468d8372c15b75229e3c62d552cc44577436662
Zeiad Haggag <zeiad.haggag2016@gmail.com>
2022-02-05 12:17:46 +0200

    ADD:detail page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
 import Header from './components/Header';
-import Home from './components/Home'
+import Home from './components/Home';
+import Details from './components/Details'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Home/>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="/details" element={<Details/>} />
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
